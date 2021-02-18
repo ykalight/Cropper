fe23fcd20a4c999d71fe60618caf43f9ed3e66c5
Young <young@youngkoh.com>
2021-02-18 15:03:09 -0600

    initial

---
+++ src/PrevOne.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// state for IMG, upload, final ok
+
+export default function PrevOne({ xpos, ypos, size }) {
+    return (
+        <div className="prev1-wrapper">
+            <div className="prev1-inner" style={{width: '450px', height: '200px', overflow: 'hidden'}}>
+                <div className="prev1-box"
+                    style={{
+                        width: `${size - 2}px`,
+                        height: `${size - 2}px`,
+                        top: `${ypos}px`,
+                        left: `${xpos}px`
+                    }}
+                ></div>
+                <img alt="" src={process.env.PUBLIC_URL + '/img/hero_01.jpg'} /> 
+            </div>
+        </div>
+    )
+}
